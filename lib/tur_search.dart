240af780b4de1344c6ef588937468bc505bc9278
taladra <612taladra@gmail.com>
2025-06-03 08:14:32 +0300

    turkish dictionary search page

--- lib/tur_search.dart
+++ lib/tur_search.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mobil_uygulama_proje/language.dart';
-import 'package:mobil_uygulama_proje/login.dart';
-import 'package:mobil_uygulama_proje/eng_search.dart';
+import 'package:mobil_uygulama_proje/main.dart';
+import 'package:mobil_uygulama_proje/result.dart';
 
 class TurSearch extends StatefulWidget {
   const TurSearch({super.key});
@@ -12,61 +11,52 @@ class TurSearch extends StatefulWidget {
 
 class _TurSearchState extends State<TurSearch> {
   String _kelime = '';
-
   TextEditingController _controller1 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    String title = 'Türkçe Sözlük - Arama';
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.deepOrange,
-          title: Text('Türkçe Sözlük - Arama',
-            style: TextStyle(
-                color: Colors.white
-            ),),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              const DrawerHeader(
-                decoration: BoxDecoration(color: Colors.deepOrangeAccent),
-                child: Text('Menu'),
-              ),
-              ListTile(
-                title: const Text('Login Page'),
-                onTap: (){
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Login()));},),
-              ListTile(
-                title: const Text('Language Selection'),
-                onTap: (){
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SelectLanguage()));},),
-              ListTile(
-                title: const Text('English Dictionary'),
-                onTap: (){
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => EngSearch()));},),
-              ListTile(
-                title: const Text('Türkçe Sözlük'),
-                onTap: (){
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => TurSearch()));},)
-            ],
-          ),
-        ),
+        appBar: CustomAppBar(title: title),
+        drawer: DrawerMenu(),
       body: Padding(
           padding: EdgeInsets.all(70),
-          child: TextField(
-            controller: _controller1,
-            decoration: InputDecoration(
-                hintText: "Bir kelime arayın: ",
-                border: OutlineInputBorder()
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Image.asset("assets/eng_dict.png",
+                  fit: BoxFit.cover,
+                  height: MediaQuery.of(context).size.height/3.5,
+                ),
+                SizedBox(height: 50),
+                TextField(
+                  controller: _controller1,
+                  decoration: InputDecoration(
+                    hintText: "Kelime arayın: ",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 50),
+                Material(
+                  borderRadius: BorderRadius.circular(10),
+                  child: MaterialButton(
+                    minWidth: double.infinity,
+                    height: 55,
+                    color: Colors.indigo,
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder:
+                          (context) => ResultScreen(word: _controller1.text),));
+                    },
+                    child: Text('Search',
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           )
       ),
